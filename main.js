621d60994cc66d993fbf1534feaa7f2f6cb430f6
li000829 <li000829@algonquinlive.com>
2024-11-05 00:15:55 -0500

    display a high definition version of the APOD image when it is clicked.

--- main.js
+++ main.js
@@ -10,3 +10,32 @@ function toggleDropdown() {
     const dropdownMenu = document.getElementById("dropdown-menu");
     dropdownMenu.classList.toggle("show");
 }
+
+// Get the modal
+const modal = document.getElementById("imageModal");
+
+const img = document.getElementById("apodImage");
+const modalImg = document.getElementById("hdImage");
+const captionText = document.getElementById("caption");
+
+img.onclick = function() {
+    modal.style.display = "block";
+    modalImg.src = "/images/Astronomy Picture of the Day.jpg"; 
+    captionText.innerHTML = this.alt;
+};
+
+// Get the <span> element that closes the modal
+const span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+    modal.style.display = "none";
+};
+
+// Close modal when clicking outside the image
+modal.onclick = function(event) {
+    if (event.target === modal) {
+        modal.style.display = "none";
+    }
+};
+
